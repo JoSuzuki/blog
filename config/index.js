4e6d58bf9ec8dff4523820e701483c6e71cda164
Jonathan Suzuki <jonathan@suzuki.pro.br>
2019-08-18 21:07:24 -0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
   siteUrl: 'https://josuzuki.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'pt', // Language Tag on <html> element
   siteHeadline: 'Escrevendo, aprendendo e refletindo', // Headline for schema.org JSONLD
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  siteBanner: '/social/social.png', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Um blog, o de sempre.', // Your site description
   author: 'JoSuzuki', // Author for schemaORGJSONLD
