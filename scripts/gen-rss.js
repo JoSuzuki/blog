bdeca2e15c4c2a88005f722cf827c19004e4a4aa
JoSuzuki <jonathan@suzuki.pro.br>
2021-07-19 23:53:21 -0300

    Fix typo in suzuki

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: "JoSuzuki Blog",
-    site_url: 'https://josuzki.me',
-    feed_url: 'https://josuzki.me/feed.xml'
+    site_url: 'https://josuzuki.me',
+    feed_url: 'https://josuzuki.me/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
