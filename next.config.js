a212b31b874198440dadbaa9b10f5ab3afaaff9a
JoSuzuki <jonathan@suzuki.pro.br>
2021-07-20 01:18:20 -0300

    try 1

--- next.config.js
+++ next.config.js
@@ -1,2 +1,19 @@
 const withNextra = require('nextra')('./nextra-theme/theme.tsx')
-module.exports = withNextra()
+module.exports = withNextra({
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && isServer) {
+      const originalEntry = config.entry
+
+      config.entry = async () => {
+        const entries = { ...(await originalEntry()) }
+
+        // These scripts can import components from the app and use ES modules
+        entries['./scripts/gen-rss.js'] = './scripts/gen-rss.js'
+
+        return entries
+      }
+    }
+
+    return config
+  },
+})
--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -1,13 +1,15 @@
-const { promises: fs } = require('fs')
-const path = require('path')
-const RSS = require('rss')
-const matter = require('gray-matter')
+import fs from 'fs/promises'
+import path from 'path'
+import RSS from 'rss'
+import matter from 'gray-matter'
+import renderToString from 'next-mdx-remote/render-to-string'
+import { MDXProvider } from '@mdx-js/react'
 
 async function generate() {
   const feed = new RSS({
-    title: "JoSuzuki Blog",
+    title: 'JoSuzuki Blog',
     site_url: 'https://josuzuki.me',
-    feed_url: 'https://josuzuki.me/feed.xml'
+    feed_url: 'https://josuzuki.me/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
@@ -17,19 +19,32 @@ async function generate() {
       if (name.startsWith('index.')) return
 
       const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
+        path.join(__dirname, '..', 'pages', 'posts', name),
       )
+
       const frontmatter = matter(content)
+      const { renderedOutput } = await renderToString(content, {
+        provider: { component: MDXProvider },
+        mdxOptions: {
+          remarkPlugins: [],
+          rehypePlugins: [],
+        },
+      })
 
       feed.item({
         title: frontmatter.data.title,
         url: '/posts/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
+        content: ReactDOMServer.renderToStaticMarkup(
+          <MDXProvider>
+            <div dangerouslySetInnerHTML={{ __html: renderedOutput }}></div>
+          </MDXProvider>,
+        ),
         categories: frontmatter.data.tags,
-        author: frontmatter.data.author
+        author: frontmatter.data.author,
       })
-    })
+    }),
   )
 
   await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
